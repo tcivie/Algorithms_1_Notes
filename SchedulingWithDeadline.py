782002633a88536f245ff48a9e3cd052df7ebc79
glebtcivie <gleb@tcivie.com>
2022-03-08 11:32:54 +0200

    Added comments

--- SchedulingWithDeadline.py
+++ SchedulingWithDeadline.py
@@ -1,4 +1,11 @@
-def sequence(d):
+def sequencel(d):
+    '''
+    Scheduling with deadline algorithm
+    Runtime: O(n^2)
+    * Initial sort time O(nlog(n))ß
+    :param d: ordered by PROFIT DEADLINE
+    :return: list of indexes to execute the tasks in
+    '''
     j = [0 for i in range(len(d))]
     d[0], j[0] = 0, 0
     k, j[1] = 1, 1
@@ -13,11 +20,11 @@ def sequence(d):
             k = k + 1
     return k, j
 
-#   [0  1   2   3   4   5   6]
-g = [0, 20, 15, 10, 7,  5,  3]
-d = [0, 3,  1,  1,  4,  2,  3]
+#   [0  1   2   3   4   5   6]  JOB
+g = [0, 20, 15, 10, 7,  5,  3]# PROFIT
+d = [0, 3,  1,  1,  4,  2,  3]# DEADLINE
 
-k, j = sequence(d)
+k, j = sequencel(d)
 print(f'Number of tasks: {k}\nOrder of tasks: {d}')
 for i in j:
     print(f'{g[i]}\t', end=',')
